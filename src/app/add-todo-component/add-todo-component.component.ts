5b0b5520842c88ef1acc90fe696df4fb7698cd05
Detlef von der Thuesen <dthuesen@gmx.de>
2017-01-06 19:47:41 +0100

    not yet good enough

--- src/app/add-todo-component/add-todo-component.component.ts
+++ src/app/add-todo-component/add-todo-component.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-add-todo-component',
-  templateUrl: './add-todo-component.component.html',
+  template: `
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3 class="panel-title">To-Do hinzufügen</h3>
+      </div>
+      <div class="panel-body">
+        <label for="todo-title">Titel:</label>
+        <input [(ngModel)]="blogPost.title" type="text" placeholder="Bitte einen Titel eingeben" class="form-control" id="todo-title">
+        <label for="todo-prio">Priorität:</label>
+        <select type="text" class="form-control" id="todo-prio" [(ngModel)]="blogPost.priority">
+          <option *ngFor="let priority of priorities" >{{priority}}</option>
+        </select>
+      </div>
+      
+      selected option: {{blogPost.priority}} <br />
+      Entered title: {{blogPost.title}}<br /><br />
+      
+      <div class="panel-footer">
+        <button type="button" class="btn btn-primary">
+          <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+          &nbsp;To-Do hinzufügen
+        </button>
+      </div>
+    </div>
+  `,
   styleUrls: ['./add-todo-component.component.css']
 })
-export class AddTodoComponentComponent implements OnInit {
+export class AddTodoComponentComponent {
+
+  priorities = [ 'niedrig', 'mittel', 'hoch' ];
+  blogPost: {} = {
+    priority: 'niedrig',
+    title: ''
+  };
+  
+  @Output() selectedPriority = new EventEmitter<string>();
 
   constructor() { }
 
-  ngOnInit() {
-  }
 
 }
--- src/app/todo-list-component/todo-list-component.component.ts
+++ src/app/todo-list-component/todo-list-component.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-todo-list-component',
-  templateUrl: './todo-list-component.component.html',
+  template: `
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3 class="panel-title">To-Do Liste</h3>
+      </div>
+        <ul class="list-group">
+          <li class="list-group-item">Cras justo odio</li>
+          <li class="list-group-item">Dapibus ac facilisis in</li>
+          <li class="list-group-item">Morbi leo risus</li>
+          <li class="list-group-item">Porta ac consectetur ac</li>
+          <li class="list-group-item">Vestibulum at eros</li>
+        </ul>
+        Input: {{blogPost.title}}
+    </div>
+    
+  `,
   styleUrls: ['./todo-list-component.component.css']
 })
-export class TodoListComponentComponent implements OnInit {
-
+export class TodoListComponentComponent {
+  
   constructor() { }
 
-  ngOnInit() {
-  }
 
 }
